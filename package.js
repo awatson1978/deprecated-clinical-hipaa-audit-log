356c4f9644ae442f7751e407d254900cf2df3e5f
Abigail Watson <awatson1978@gmail.com>
2015-11-04 14:34:59 -0800

    updated to clinical:router

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "HIPAA audit log for ClinicalFramework.",
-  version: "2.3.5",
+  version: "2.3.6",
   git: "http://github.com/clinical-meteor/clinical-hipaa-audit-log.git",
   name: "clinical:hipaa-audit-log"
 });
@@ -9,12 +9,12 @@ Package.on_use(function (api) {
   api.versionsFrom('1.1.0.2');
 
   api.use('meteor-platform');
-  api.use('iron:router@1.0.9', 'client');
   api.use('mrt:moment@2.8.1', 'client');
   api.use('grove:less@0.1.1', 'client');
   api.use('fortawesome:fontawesome@4.4.0', 'client');
 
   api.use('clinical:auto-resizing@0.1.2', 'client');
+  api.use('clinical:router@2.0.13');
 
   api.addFiles('lib/HipaaLogger.js', ["client", "server"]);
   api.addFiles('lib/HipaaAuditLog.js', ["client", "server"]);
@@ -49,7 +49,7 @@ Package.onTest(function (api) {
   api.use('tinytest');
 
   api.use('meteor-platform@1.2.2');
-  api.use('iron:router@1.0.4', 'client');
+  api.use('clinical:router@2.0.13', 'client');
   api.use('mrt:moment@2.8.1', 'client');
   api.use('grove:less@0.1.1', 'client');
   api.use('fortawesome:fontawesome@4.4.0', 'client');
